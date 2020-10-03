df0872e234d1e9ca334f057430092683cefaf82a
dziljalal1 <dziljalal1@gmail.com>
2020-10-03 18:58:50 +0700

    Initial Commit

---
+++ resources/views/layout/backend.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>App Name - @yield('title')</title>
+
+</head>
+<body>
+@section('sidebar')
+    This is the master sidebar.
+@show
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
