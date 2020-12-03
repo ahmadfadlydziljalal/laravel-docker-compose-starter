f42518f4f6991015355569adaff0f6c5251fd7cf
dziljalal1 <dziljalal1@gmail.com>
2020-12-04 00:06:42 +0700

    Tailwind Css under PostCSS8

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
 
-    <title>App Name - @yield('title')</title>
-
+    <link rel="stylesheet" href="{{ asset('css/app.css')  }}}">
 </head>
-<body>
-@section('sidebar')
-    This is the master sidebar.
-@show
 
-<div class="container">
-    @yield('content')
-</div>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
+
 </html>
